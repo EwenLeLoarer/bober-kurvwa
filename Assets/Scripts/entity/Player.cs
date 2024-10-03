c47a3c3ded6798557768d8a112cbe47c2f515ebe
EwenLeLoarer <ewen.leloarer1@gmail.com>
2024-10-03 22:23:28 +0200

    Add bobers to you when you touch them, in circular form if there is more than one

---
+++ Assets/Scripts/entity/Player.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public List<Transform> bobers = new List<Transform>();
+    public Transform centerPoint; 
+    public float radius = 5f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider){
+        Debug.Log(collider);
+        if(collider.GetComponent<bober>() != null){
+            collider.transform.parent = this.transform;
+            bobers.Add(collider.transform);
+            Harmonize();
+            CenterChildren();
+        }
+    }
+
+    private void CenterChildren (){
+         if (transform.childCount == 0)
+        {
+            Debug.LogWarning("No children to center on.");
+            return;
+        }
+
+        Vector3 center = Vector3.zero;
+        foreach (Transform child in transform)
+        {
+            center += child.position;
+        }
+        
+        center /= transform.childCount;
+
+        // Optionally, adjust the position of the children as well so they maintain the same relative position
+        Vector3 parentOldPosition = transform.position;
+        transform.position = center;
+
+        foreach (Transform child in transform)
+        {
+            child.position -= (center - parentOldPosition);
+        }
+    }
+
+    private void Harmonize()
+    {
+        int count = bobers.Count;
+        float angleStep = 360f / count;
+
+        for (int i = 0; i < count; i++){
+            float angle = i * angleStep * Mathf.Deg2Rad;
+
+            Vector3 planePosition = new Vector3(
+                Mathf.Cos(angle) * radius + centerPoint.position.x,
+                centerPoint.position.y,
+                Mathf.Sin(angle) * radius + centerPoint.position.z
+            );
+
+            bobers[i].position = planePosition;
+            
+        }
+    }
+}
